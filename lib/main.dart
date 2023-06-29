400cc17e2cf7a09018730335e40376dccb8cd699
nsenji <jerrysyre@gmail.com>
2023-06-29 13:02:57 +0300

    consfigured some constants and the fonts for overall

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,9 @@ import 'package:cinema_tikets/notifiers/for_acacia/location.dart';
 import 'package:cinema_tikets/notifiers/for_acacia/time.dart';
 import 'package:cinema_tikets/notifiers/for_acacia/title_image_provider.dart';
 import 'package:cinema_tikets/pages/auth_pages/wrapper.dart';
+import 'package:cinema_tikets/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'api/for_acacia/seat_methods.dart';
 import 'firebase_options.dart';
@@ -72,16 +74,16 @@ class _MyAppState extends State<MyApp> {
   ColorScheme lightThemeColors(context) {
     return const ColorScheme(
       brightness: Brightness.light,
-      primary: Color(0xFFFFFFFF),
+      primary:  Color(0xFFEF5B6B),
       onPrimary: Color(0xFFFFFFFF),
-      secondary: Color(0xFFBBBBBB),
-      onSecondary: Color(0xFFEAEAEA),
+      secondary: Colors.grey,
+      onSecondary: Color(0xFFFFFFFF),
       error: Color(0xFFF32424),
       onError: Color(0xFFF32424),
       background: Color(0xFFFFFFFF),
-      onBackground: Color(0xFFFFFFFF),
+      onBackground: Color(0xFF4D4C4C),
       surface: Color(0xFFFFFFFF),
-      onSurface: Color.fromARGB(255, 255, 255, 255),
+      onSurface: Color(0xFF4D4C4C),
     );
   }
 
@@ -103,21 +105,31 @@ class _MyAppState extends State<MyApp> {
 
   ThemeData lightThemeData(BuildContext context) {
     return ThemeData(
+      primaryColor:const Color(0xFFEF5B6B) ,
+      useMaterial3: true,
+      dialogBackgroundColor:const Color(0xFFFFFFFF) ,
+      cardColor: const Color(0xFFFFFFFF),
+      fontFamily: Constants.appFont.fontFamily,
       scaffoldBackgroundColor: lightThemeColors(context).background,
       textTheme: textTheme(context),
       appBarTheme: appBarTheme(context),
-      inputDecorationTheme: inputDecorationData(context),
       colorScheme: lightThemeColors(context),
-      
+      iconTheme: const IconThemeData(color: Color(0xFF4D4C4C)),
+      elevatedButtonTheme:  ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xFFEF5B6B)
+        )
+      )
     );
   }
 
   ThemeData darkThemeData(BuildContext context) {
     return ThemeData(
+      useMaterial3: true,
       scaffoldBackgroundColor: lightThemeColors(context).background,
+      fontFamily: Constants.appFont.fontFamily,
       textTheme: textTheme(context),
       appBarTheme: appBarTheme(context),
-      inputDecorationTheme: inputDecorationData(context),
       colorScheme: darkThemeColors(context),
     );
   }
@@ -125,229 +137,194 @@ class _MyAppState extends State<MyApp> {
   appBarTheme(context) {
     return AppBarTheme(
       backgroundColor: lightThemeColors(context).background,
-      titleTextStyle: Theme.of(context)
-          .textTheme
-          .headline5
-          ?.copyWith(color: Theme.of(context).colorScheme.primary),
+      titleTextStyle: textTheme(context).bodyLarge,
     );
   }
 
-  InputDecorationTheme inputDecorationData(context) {
-    return InputDecorationTheme(
-      filled: true,
-      fillColor: lightThemeColors(context).onBackground,
-      hintStyle: Theme.of(context)
-          .textTheme
-          .bodyText1
-          ?.copyWith(color: lightThemeColors(context).onSecondary),
-    );
-  }
+  
 
   TextTheme textTheme(context) {
     return TextTheme(
-        labelLarge: TextStyle(color: lightThemeColors(context).primary));
+      labelLarge:
+          Constants.appFont.copyWith(fontWeight: FontWeight.bold, fontSize: 24),
+      labelMedium:
+          Constants.appFont.copyWith(fontWeight: FontWeight.normal, fontSize: 14),
+      labelSmall:
+          Constants.appFont.copyWith(fontWeight: FontWeight.normal, fontSize: 10),
+      bodySmall: Constants.appFont.copyWith(
+          fontWeight: FontWeight.bold,
+          fontSize: 10,
+          color: const Color(0x2983FFFF)),
+    );
   }
-  
-}
-
-
-@immutable
-class Constants {
-  const Constants._();
-
-  /// The main orange-red color used for theming the app.
-  static const Color primaryColor = Color(0xFFf03400);
-
-  /// The color value for red color in the app.
-  static const Color redColor = Color(0xFFed0000);
-
-  /// The color value for orange color in the app.
-  static const Color orangeColor = Color(0xFFf04f00);
-
-  /// The color value for rating stars in the app.
-  static const Color starsColor = Color(0xFFf78040);
-
-  /// The color value for dark grey skeleton containers in the app.
-  static const Color darkSkeletonColor = Color(0xFF656565);
-
-  /// The color value for light grey skeleton containers in the app.
-  static const Color lightSkeletonColor = Colors.grey;
-
-  /// The red [LinearGradient] for buttons in the app.
-  static const Gradient buttonGradientRed = LinearGradient(
-    colors: [primaryColor, redColor],
-  );
-
-  /// The orange [LinearGradient] for buttons in the app.
-  static const Gradient buttonGradientOrange = LinearGradient(
-    colors: [orangeColor, redColor],
-  );
-
-  /// The orange [LinearGradient] for disabled buttons in the app.
-  static const Gradient buttonGradientGrey = LinearGradient(
-    colors: [textGreyColor, scaffoldGreyColor],
-  );
-
-  /// The price for a single seat ticket
-  static const double ticketPrice = 800;
-
-  /// The white [LinearGradient] for fading movies carousel in the app.
-  static const Gradient movieCarouselGradient = LinearGradient(
-    begin: Alignment.bottomCenter,
-    end: Alignment.topCenter,
-    stops: [0.3, 0.6, 1],
-    colors: [
-      Color.fromRGBO(255, 255, 255, 0.95),
-      Colors.white70,
-      Colors.transparent,
-    ],
-  );
-
-  /// The black [LinearGradient] used to overlay movie backgrounds in the app.
-  static const Gradient blackOverlayGradient = LinearGradient(
-    begin: Alignment.topCenter,
-    end: Alignment.bottomCenter,
-    stops: [0.2, 0.5, 0.7, 1],
-    colors: [
-      Color.fromRGBO(0, 0, 0, 0.6),
-      Color.fromRGBO(0, 0, 0, 0.45),
-      Color.fromRGBO(0, 0, 0, 0.3),
-      Colors.transparent,
-    ],
-  );
-
-  /// The color value for dark grey buttons in the app.
-  static const Color buttonGreyColor = Color(0xFF1c1c1c);
-
-  /// The color value for dark grey scaffold in the app.
-  static const Color scaffoldColor = Color(0xFF141414);
-
-  /// The color value for light grey scaffold in the app.
-  static const Color scaffoldGreyColor = Color(0xFF2b2b2b);
-
-  /// The color value for light grey text in the app.
-  static const Color textGreyColor = Color(0xFF949494);
-
-  /// The color value for white text in the app.
-  static const Color textWhite80Color = Color(0xFFf2f2f2);
-
-  /// The color value for dark grey [CustomDialog] in the app.
-  static const Color barrierColor = Colors.black87;
-
-  /// The color value for light grey [CustomDialog] in the app.
-  static const Color barrierColorLight = Color(0xBF000000);
-
-  /// The TextStyle for Lato font in the app.
-  static TextStyle latoFont = GoogleFonts.lato().copyWith(color: Colors.black);
-
-  /// The TextStyle for Poppins font in the app.
-  static TextStyle poppinsFont = GoogleFonts.poppins().copyWith(
-    color: textWhite80Color,
-  );
-
-  /// The TextStyle for Roboto font in the app.
-  static TextStyle robotoFont = GoogleFonts.roboto();
-
-  /// The default [Duration] value for animations in the app.
-  static const Duration defaultAnimationDuration = Duration(milliseconds: 300);
-
-  /// The value for bottom padding of buttons in the app.
-  /// It is measured from the bottom of the screen, that is
-  /// behind the android system navigation.
-  /// Used to prevent overlapping of android navigation with the button.
-  static const double bottomInsets = 65;
-
-  /// The value for a smaller bottom padding of buttons in the app.
-  /// It is measured from the bottom of the screen, that is
-  /// behind the android system navigation.
-  /// Used to prevent overlapping of android navigation with the button.
-  static const double bottomInsetsLow = 48;
-
-  /// The max number of rows a theater can contain
-  static const int maxSeatRows = 12;
-
-  
-
-  /// The error message for invalid credit card expiry input.
-  static const invalidCreditCardExpiryError = 'Please enter a valid expiry date';
-
-  static T? toNull<T>(Object? _) => null;
 }
 
 
 
-@immutable
-class CustomTheme {
-  const CustomTheme._();
 
-  /// The main starting theme for the app.
-  ///
-  /// Sets the following defaults:
-  /// * primaryColor: [Constants.primaryColor],
-  /// * scaffoldBackgroundColor: [Constants.scaffoldColor],
-  /// * fontFamily: [Constants.poppinsFont].fontFamily,
-  /// * iconTheme: [Colors.white] for default icon
-  /// * textButtonTheme: [TextButtonTheme] without the default padding,
-  static late final mainTheme = ThemeData(
-    primaryColor: Constants.primaryColor,
-    colorScheme: const ColorScheme.dark().copyWith(
-      primary: Constants.primaryColor,
-      primaryVariant: Constants.primaryColor,
-      secondary: Constants.primaryColor,
-      secondaryVariant: Constants.primaryColor,
-    ),
-    scaffoldBackgroundColor: Constants.scaffoldColor,
-    fontFamily: Constants.poppinsFont.fontFamily,
-    textTheme: TextTheme(
-      headline1: Constants.poppinsFont.copyWith(
-        fontWeight: FontWeight.bold,
-        fontSize: 50,
-        height: 1.15,
-      ),
-      headline2: Constants.latoFont.copyWith(
-        fontWeight: FontWeight.bold,
-        fontSize: 50,
-        height: 1.15,
-      ),
-      headline3: Constants.poppinsFont.copyWith(
-        fontWeight: FontWeight.w400,
-        fontSize: 26,
-        height: 1.15,
-      ),
-      headline4: Constants.latoFont.copyWith(
-        fontWeight: FontWeight.w400,
-        fontSize: 26,
-        height: 1.15,
-      ),
-      headline5: Constants.poppinsFont.copyWith(
-        fontWeight: FontWeight.w300,
-        fontSize: 20,
-        height: 1.15,
-      ),
-      headline6: Constants.latoFont.copyWith(
-        fontWeight: FontWeight.w300,
-        fontSize: 20,
-        height: 1.15,
-      ),
-      subtitle1: Constants.poppinsFont.copyWith(
-        fontWeight: FontWeight.w200,
-      ),
-      subtitle2: Constants.latoFont.copyWith(
-        fontWeight: FontWeight.w200,
-      ),
-      bodyText1: Constants.poppinsFont.copyWith(
-        fontWeight: FontWeight.w400,
-      ),
-      bodyText2: Constants.latoFont.copyWith(
-        fontWeight: FontWeight.w400,
-      ),
-    ),
-    textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(padding: const EdgeInsets.all(0)),
-    ),
-    iconTheme: const IconThemeData(color: Colors.white),
-  );
-}
+// @immutable
+// class Constants {
+//   const Constants._();
+
+//   /// The main orange-red color used for theming the app.
+//   static const Color primaryColor = Color(0xFFf03400);
+
+//   /// The color value for red color in the app.
+//   static const Color redColor = Color(0xFFed0000);
+
+//   /// The color value for orange color in the app.
+//   static const Color orangeColor = Color(0xFFf04f00);
+
+//   /// The color value for rating stars in the app.
+//   static const Color starsColor = Color(0xFFf78040);
+
+//   /// The color value for dark grey skeleton containers in the app.
+//   static const Color darkSkeletonColor = Color(0xFF656565);
+
+//   /// The color value for light grey skeleton containers in the app.
+//   static const Color lightSkeletonColor = Colors.grey;
+
+//   /// The red [LinearGradient] for buttons in the app.
+//   static const Gradient buttonGradientRed = LinearGradient(
+//     colors: [primaryColor, redColor],
+//   );
+
+//   /// The orange [LinearGradient] for buttons in the app.
+//   static const Gradient buttonGradientOrange = LinearGradient(
+//     colors: [orangeColor, redColor],
+//   );
+
+//   /// The orange [LinearGradient] for disabled buttons in the app.
+//   static const Gradient buttonGradientGrey = LinearGradient(
+//     colors: [textGreyColor, scaffoldGreyColor],
+//   );
+
+//   /// The price for a single seat ticket
+//   static const double ticketPrice = 800;
+
+//   /// The color value for dark grey buttons in the app.
+//   static const Color buttonGreyColor = Color(0xFF1c1c1c);
+
+//   /// The color value for dark grey scaffold in the app.
+//   static const Color scaffoldColor = Color(0xFF141414);
+
+//   /// The color value for light grey scaffold in the app.
+//   static const Color scaffoldGreyColor = Color(0xFF2b2b2b);
+
+//   /// The color value for light grey text in the app.
+//   static const Color textGreyColor = Color(0xFF949494);
+
+//   /// The color value for white text in the app.
+//   static const Color textWhite80Color = Color(0xFFf2f2f2);
+
+//   /// The color value for dark grey [CustomDialog] in the app.
+//   static const Color barrierColor = Colors.black87;
+
+//   /// The color value for light grey [CustomDialog] in the app.
+//   static const Color barrierColorLight = Color(0xBF000000);
+
+//   /// The TextStyle for Lato font in the app.
+//   static TextStyle latoFont = GoogleFonts.lato().copyWith(color: Colors.black);
+
+//   /// The TextStyle for Poppins font in the app.
+//   static TextStyle poppinsFont = GoogleFonts.poppins().copyWith(
+//     color: textWhite80Color,
+//   );
+
+//   /// The TextStyle for Roboto font in the app.
+//   static TextStyle robotoFont = GoogleFonts.roboto();
+
+//   /// The default [Duration] value for animations in the app.
+//   static const Duration defaultAnimationDuration = Duration(milliseconds: 300);
+
+//   /// The value for bottom padding of buttons in the app.
+//   /// It is measured from the bottom of the screen, that is
+//   /// behind the android system navigation.
+//   /// Used to prevent overlapping of android navigation with the button.
+//   static const double bottomInsets = 65;
+
+//   /// The value for a smaller bottom padding of buttons in the app.
+//   /// It is measured from the bottom of the screen, that is
+//   /// behind the android system navigation.
+//   /// Used to prevent overlapping of android navigation with the button.
+//   static const double bottomInsetsLow = 48;
+
+//   static TextStyle appFont =
+//       GoogleFonts.roboto().copyWith(color: const Color(0xFF4D4C4C));
+// }
+
+// @immutable
+// class CustomTheme {
+//   const CustomTheme._();
+
+//   /// The main starting theme for the app.
+//   ///
+//   /// Sets the following defaults:
+//   /// * primaryColor: [Constants.primaryColor],
+//   /// * scaffoldBackgroundColor: [Constants.scaffoldColor],
+//   /// * fontFamily: [Constants.poppinsFont].fontFamily,
+//   /// * iconTheme: [Colors.white] for default icon
+//   /// * textButtonTheme: [TextButtonTheme] without the default padding,
+//   static late final mainTheme = ThemeData(
+//     primaryColor: Constants.primaryColor,
+//     colorScheme: const ColorScheme.dark().copyWith(
+//       primary: Constants.primaryColor,
+//       primaryVariant: Constants.primaryColor,
+//       secondary: Constants.primaryColor,
+//       secondaryVariant: Constants.primaryColor,
+//     ),
+//     scaffoldBackgroundColor: Constants.scaffoldColor,
+//     fontFamily: Constants.poppinsFont.fontFamily,
+//     textTheme: TextTheme(
+//       headline1: Constants.poppinsFont.copyWith(
+//         fontWeight: FontWeight.bold,
+//         fontSize: 50,
+//         height: 1.15,
+//       ),
+//       headline2: Constants.latoFont.copyWith(
+//         fontWeight: FontWeight.bold,
+//         fontSize: 50,
+//         height: 1.15,
+//       ),
+//       headline3: Constants.poppinsFont.copyWith(
+//         fontWeight: FontWeight.w400,
+//         fontSize: 26,
+//         height: 1.15,
+//       ),
+//       headline4: Constants.latoFont.copyWith(
+//         fontWeight: FontWeight.w400,
+//         fontSize: 26,
+//         height: 1.15,
+//       ),
+//       headline5: Constants.poppinsFont.copyWith(
+//         fontWeight: FontWeight.w300,
+//         fontSize: 20,
+//         height: 1.15,
+//       ),
+//       headline6: Constants.latoFont.copyWith(
+//         fontWeight: FontWeight.w300,
+//         fontSize: 20,
+//         height: 1.15,
+//       ),
+//       subtitle1: Constants.poppinsFont.copyWith(
+//         fontWeight: FontWeight.w200,
+//       ),
+//       subtitle2: Constants.latoFont.copyWith(
+//         fontWeight: FontWeight.w200,
+//       ),
+//       bodyText1: Constants.poppinsFont.copyWith(
+//         fontWeight: FontWeight.w400,
+//       ),
+//       bodyText2: Constants.latoFont.copyWith(
+//         fontWeight: FontWeight.w400,
+//       ),
+//     ),
+//     textButtonTheme: TextButtonThemeData(
+//       style: TextButton.styleFrom(padding: const EdgeInsets.all(0)),
+//     ),
+//     iconTheme: const IconThemeData(color: Colors.white),
+//   );
+// }
 
 
 
---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Constants {
+  Constants._();
+
+  static  TextStyle appFont =
+     GoogleFonts.roboto().copyWith(color: const Color(0xFF4D4C4C));
+}
